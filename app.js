571761fd0b37e9a1f95a211650e686ca3136532f
Kshettij <kshtu@Kshettijs-Mac-mini.local>
2025-05-19 17:10:57 +0530

    Array Creation to store MetaData about Songs

---
+++ app.js
@@ -0,0 +1,111 @@
+const music  = new Audio('vande.mp3')
+
+
+// Create Array
+const songs = [
+    {
+        id: '1',
+        songName : ` On My Way <br>
+        <div class="subtitle"> Alan Walker</div>`,
+        poster: "img/1.jpg"
+    },
+    {
+        id : '2',
+        songName:  ` Alan Walker-Fade <br>
+        <div class="subtitle"> Alan Walker </div>`,
+        poster: "img/2.jpg"
+    },
+    // all object type
+    {
+        id : '3',
+        songName: `Cartoon - On & on <br>
+        <div class="subtitle> Daniel Levi</div>`,
+        poster: "img/3.jpg",
+    },
+    {
+        id : "4",
+        songName:  `Warrio -  Mortals <br>
+        <div class="subtitle">Mortals</div>`,
+        poster: "img/4.jpg",
+
+    },
+    {
+        id : "5",
+        songName:  `Ertugrul Gazi <br>
+        <div class="subtitle">Ertugrul</div>`,
+        poster: "img/5.jpg",
+
+    },
+    {
+        id : "6",
+        songName:  `Electronic Music <br>
+        <div class="subtitle">Electro</div>`,
+        poster: "img/6.jpg",
+
+    },
+    {
+        id : "7",
+        songName:  `Agar Tum Sath Ho <br>
+        <div class="subtitle">Tamashaa</div>`,
+        poster: "img/7.jpg",
+
+    },
+    {
+        id : "8",
+        songName:  `Suna Hai <br>
+        <div class="subtitle">Neha Kakker</div>`,
+        poster: "img/8.jpg",
+
+    },
+    {
+        id : "9",
+        songName:  `Dilber<br>
+        <div class="subtitle">Satyameva Jayate</div>`,
+        poster: "img/9.jpg",
+
+    },
+    {
+        id : "10",
+        songName:  `Duniya <br>
+        <div class="subtitle">Luka Chuppi</div>`,
+        poster: "img/10.jpg",
+
+    },
+    {
+        id : "11",
+        songName:  `Lagdi Lahore Di <br>
+        <div class="subtitle">Street Dancer 3D</div>`,
+        poster: "img/11.jpg",
+
+    },
+    {
+        id : "12",
+        songName:  `Putt Jatt Da <br>
+        <div class="subtitle">Putt Jatt Da</div>`,
+        poster: "img/12.jpg",
+
+    },
+    {
+        id : "13",
+        songName:  `Baarishein <br>
+        <div class="subtitle">Atif Aslam</div>`,
+        poster: "img/13.jpg",
+
+    },
+    {
+        id : "14",
+        songName:  `Vaaste <br>
+        <div class="subtitle">Dhvani Bhanushali</div>`,
+        poster: "img/14.jpg",
+
+    },
+    {
+        id : "15",
+        songName:  `Lut Gaye <br>
+        <div class="subtitle">Jubin Nautiyal</div>`,
+        poster: "img/15.jpg",
+
+    },
+
+    
+]
